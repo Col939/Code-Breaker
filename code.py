26d1215158190cf779d0bd39042cfb2dc2247bca
Col939 <87954615+Col939@users.noreply.github.com>
2021-09-14 15:15:42 -0400

    Update code.py

--- code.py
+++ code.py
@@ -5,9 +5,16 @@ code = ""
 def mainGame():
   global code
   if len(code) == 3:
+    print("3 Numbers \n")
     print("Number 1 times Number 3 is " + str(int(code[0]) * int(code[2])))
     print("The difference between Number 1 * Number 3 and Number 2 is " + str( ( int(code[0]) * int(code[2]) ) - int(code[1]) ) )
     print("Number 3 plus Number 2 is " + str(int(code[2]) + int(code[1])) )
+    
+  if len(code) == 4:
+    print("4 Numbers \n")
+    print("Number 1 plus Number 2 is " + str( int(code[0]) + int(code[1]) ) )
+    print("Number 3 minus number 2 is " + str( int(code[2]) - int(code[1]) ) )
+    print("Number 4 times Number 3 is " + str( int(code[3]) * int(code[2]) ) )
   
   i = input("Code: ")
   
