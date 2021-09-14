23c4eaa89c7666d78589511ebb996071bbdf9e47
Col939 <87954615+Col939@users.noreply.github.com>
2021-09-14 10:39:51 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,21 @@
 import random
 
-code = 0
+code = ""
 
-def startGame()
+def mainGame():
+  global code
+  if len(code) == 3:
+    print("Number 1 times Number 3 is " + str(int(code[0]) * int(code[2])))
+    print("The difference between Number 2 and Number 1 * Number 3 is " + str((int(code[0]) * int(code[2])) - int(code[1]))
+
+def startGame():
+    
   global code
   
-  for x in range(random.randint(3, 6):
-    code += random.randint(0,9)
-  print(code)  
+  for x in range(random.randint(3, 6)):
+    code += str(random.randint(0,9))
+  print(code)
+  
+  mainGame()
 startGame()
+
